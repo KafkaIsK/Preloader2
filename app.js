97f3d46df87f88a80125d0c10f1e581e20f4638e
KafkaIsK <richlam4@gmail.com>
2021-07-27 22:02:56 +0100

    loading hidden

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 window.addEventListener('load', () => {
     const preload = document.querySelector('.preloader');
     preload.classList.add('preload-finish');
+
+    const loading = document.querySelector('.loading');
+    loading.classList.remove('loading');
 });
